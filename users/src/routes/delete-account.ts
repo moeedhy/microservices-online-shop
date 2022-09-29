b2c809f32a27a4442cbe8ed4323f2ea97b5415b6
moeedhy <mhg375@yahoo.com>
2022-09-29 15:46:32 +0330

    Add delete user route

--- users/src/routes/delete-account.ts
+++ users/src/routes/delete-account.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { NotFoundError, requireAuth } from "@moeed/common";
+import { isAdmin } from "@moeed/common";
+import User from "../models/user";
 
 const router = Router();
 
-router.delete('/delete')
+router.delete("/delete:id", requireAuth, isAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) throw new NotFoundError("User not found");
+    await user.destroy();
+    res
+      .status(200)
+      .send({ message: "User deleted", data: { user: { id: user.id } } });
+  } catch (e) {
+    next(e);
+  }
+});
